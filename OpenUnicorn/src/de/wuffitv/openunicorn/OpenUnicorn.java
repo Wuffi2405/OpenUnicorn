c6499460b6b163e3538ff440f0090b083611c680
Wuffi2405 <wuffi2405@gmail.com>
2017-03-09 19:16:36 +0100

    update Dev

--- OpenUnicorn/src/de/wuffitv/openunicorn/OpenUnicorn.java
+++ OpenUnicorn/src/de/wuffitv/openunicorn/OpenUnicorn.java
@@ -2,19 +2,8 @@ package de.wuffitv.openunicorn;
 
 public class OpenUnicorn {
 	
-	int i;
-	
-	//ich bin dumm
-	
 	public static void main(String[] args) {
 		
-		
-		//awdawdaw
-		/*
-		 * 
-		 * LOLOL
-		 * 
-		 */
 	}
 	
 }
