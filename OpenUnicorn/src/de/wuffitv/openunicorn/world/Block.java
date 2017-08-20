c7385480514a2226f5355939c69752acd8368699
FraFrieFa <fabius2001@hotmail.de>
2017-08-20 11:24:27 +0200

    added block class

---
+++ OpenUnicorn/src/de/wuffitv/openunicorn/world/Block.java
@@ -0,0 +1,23 @@
+package de.wuffitv.openunicorn.world;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import com.sun.javafx.geom.Rectangle;
+
+public class Block extends Rectangle {
+
+	public int x, y, width, height;
+
+	public Block(int x, int y, int width, int height, BufferedImage image) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public void render(Graphics g) {
+		g.fillRect(x, y, width, height);
+	}
+
+}
